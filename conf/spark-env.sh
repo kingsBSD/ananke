6ba63f3653e168a3c9c90c4e00fbda63afda41c5
Giles Richard Greenway <augeas@gmail.com>
2016-07-06 12:12:38 +0100

    modified:   conf/spark-env.sh

--- conf/spark-env.sh
+++ conf/spark-env.sh
@@ -1,2 +1,4 @@
 export SPARK_HOME=/spark-1.6.1-bin-hadoop2.6 
 export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.9-src.zip
+export PYSPARK_PYTHON=/usr/bin/python3       
+export PYSPARK_DRIVER_PYTHON=/usr/bin/ipython
