e3dd0b6e09093d5d4a37b82c963bcf9d4c94cd37
Giles Richard Greenway <augeas@gmail.com>
2016-07-26 01:46:09 +0100

    modified:   ananke/ipgetter.py

--- ananke/ipgetter.py
+++ ananke/ipgetter.py
@@ -7,7 +7,8 @@ import re
 def get_ip():
     f = os.popen('ifconfig')
     for iface in [' '.join(i) for i in iter(lambda: list(itertools.takewhile(lambda l: not l.isspace(),f)), [])]:
-        if re.findall('^(eth|wlan)[0-9]',iface) and re.findall('RUNNING',iface):
+        if re.findall('^(eth|wlan|enp0s)[0-9]',iface) and re.findall('RUNNING',iface):
+            ip = re.findall('(?<=inet\s)[0-9]',iface) and re.findall('RUNNING',iface):
             ip = re.findall('(?<=inet\saddr:)[0-9\.]+',iface)
             if ip:
                 return ip[0]
