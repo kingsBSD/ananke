12d7cf5719cc551151c69a260c63a62bdc93d436
Giles Richard Greenway <augeas@gmail.com>
2016-08-26 18:54:57 +0100

    Notebook works for Spark 2.0...  On branch master 	modified:   ananke/app.py 	modified:   ananke/ipgetter.py 	modified:   conf/spark-env.sh 	modified:   scripts/spark_jupyter_standalone

--- conf/spark-env.sh
+++ conf/spark-env.sh
@@ -1,4 +1,8 @@
 export SPARK_HOME=/spark-2.0.0-bin-hadoop2.6
 export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.9-src.zip
 export PYSPARK_PYTHON=/usr/bin/python3.5
-export PYSPARK_DRIVER_PYTHON=ipython3
+export PYSPARK_DRIVER_PYTHON=/usr/local/bin/jupyter
+export PYSPARK_DRIVER_PYTHON_OPTS="notebook --NotebookApp.open_browser=False --NotebookApp.ip='0.0.0.0' --NotebookApp.port=8888"
+
+
+
