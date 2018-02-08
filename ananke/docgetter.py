f4ed2268931bcaa73a188f9335f197975adbb716
Giles Richard Greenway <augeas@gmail.com>
2018-02-08 21:51:24 +0000

    modified:   ananke/docgetter.py

--- ananke/docgetter.py
+++ ananke/docgetter.py
@@ -3,15 +3,12 @@ import json
 import pip
 import pydoc
 
-from ipgetter import get_ip
-
-def render_doc(doc,ip):
+def render_doc(doc):
     return {'name': doc.project_name,
-        'pypi':'https://pypi.python.org/pypi/'+doc.project_name+'/'+doc.version,
-        'local':'http://'+ip+':9000/'+doc.project_name.lower()+'.html'}
+        'pypi':"https://pypi.python.org/pypi/"+doc.project_name+"/"+doc.version,
+        'local':"http://localhost:9000/"+doc.project_name.lower()+'.html'}
 
 def get_docs():
-    ip = get_ip()
-    return [render_doc(doc,ip) for doc in sorted(pip.get_installed_distributions())]
+    return [render_doc(doc) for doc in sorted(pip.get_installed_distributions())]
 
     
